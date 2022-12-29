c5cc6eac15854b53c5591d31ac1c3ca59272bd4a
Jason Miller <jasonmiller.dev@gmail.com>
2022-12-29 11:04:17 +1100

    basic project layout and set up

---
+++ src/AddressForm.js
@@ -0,0 +1,65 @@
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+
+export default function AddressForm() {
+  return (
+    <React.Fragment>
+      <Typography variant="h6" gutterBottom>
+        Your Contact Details
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            id="firstName"
+            name="firstName"
+            label="First name"
+            fullWidth
+            autoComplete="given-name"
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            id="lastName"
+            name="lastName"
+            label="Last name"
+            fullWidth
+            autoComplete="family-name"
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            id="address1"
+            name="address1"
+            label="Contact Number"
+            fullWidth
+            autoComplete="shipping address-line1"
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={12} sm="6">
+          <TextField
+            id="address2"
+            name="address2"
+            label="Email Address"
+            fullWidth
+            autoComplete="shipping address-line2"
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}></Grid>
+        <Grid item xs={12} sm={6}></Grid>
+        <Grid item xs={12} sm={6}></Grid>
+        <Grid item xs={12} sm={6}></Grid>
+      </Grid>
+    </React.Fragment>
+  );
+}
